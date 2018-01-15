e85e093640f0f78cad17d3957952882d682ae6f4
Sergey Zelenov <github@zelenov.su>
2018-01-15 14:32:36 +0100

    Update SeriesActions.js

--- scripts/actions/SeriesActions.js
+++ scripts/actions/SeriesActions.js
@@ -4,7 +4,7 @@ export default class SeriesActions extends Actions {
 
   async getAllSeries() {
     let response;
-    let url = 'https://api.mtstv.ru/v1/collections/all_series/items?offset=0&count=10&access_token=CU5QXs77_8SoNAV-18H3zyXjVx5bMGkRv401UAG6Mrk';
+    let url = process.env.API_URL;
     try {
 
       response = await fetch(url);
